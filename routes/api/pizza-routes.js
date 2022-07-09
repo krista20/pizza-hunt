38cf532388a0ccae7200305e00b5c2cb458a0934
Rush <kriscookies20@gmail.com>
2022-07-09 05:26:13 -0700

    fixing files

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,22 +1,18 @@
 const router = require('express').Router();
 const {
-    getAllPizza,
-    getPizzaById,
-    createPizza,
-    updatePizza,
-    deletePizza
-  } = require('../../controllers/pizza-controller');
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza
+} = require('../../controllers/pizza-controller');
 
-
-
-// this is setting up for Express.js
-// Set up GET all and POST at /api/pizzas
+// /api/pizzas
 router
   .route('/')
-  .get()
-  .post();
+  .get(getAllPizza)
+  .post(createPizza);
 
-// Set up GET one, PUT, and DELETE at /api/pizzas/:id
 // /api/pizzas/:id
 router
   .route('/:id')
@@ -24,19 +20,4 @@ router
   .put(updatePizza)
   .delete(deletePizza);
 
-  // /api/pizzas
-router
-.route('/')
-.get(getAllPizza)
-.post(createPizza);
-
 module.exports = router;
-
-
-// can combine routes for individual HTTP methods
-    // // this code
-    // router.route('/').get(getCallbackFunction).post(postCallbackFunction);
-
-    // // is this same as this
-    // router.get('/', getCallbackFunction);
-    // router.post('/' postCallbackFunction);
