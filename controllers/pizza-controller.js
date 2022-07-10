828270aa3bef0ac467d7f26aa6cb5a7265015b1b
Rush <kriscookies20@gmail.com>
2022-07-09 21:04:13 -0700

    creating and testing routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,20 +4,42 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+  .populate({
+    path: 'comments',
+    // "-" sign means we dont want it returned. "select" option inside populate() tells Mongoose we dont
+    // care about the "__v" field on comments
+    select: '-__v'
+  })
+  //select method to not include pizza __v field
+  .select('-__v')
+  //sort get the newest pizza in desc order
+  .sort({ _id: -1 })
+  .then(dbPizzaData => res.json(dbPizzaData))
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
-        res.sendStatus(400);
+        res.status(400).json(err);
       });
   },
 
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then(dbPizzaData => res.json(dbPizzaData))
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
       .catch(err => {
         console.log(err);
-        res.sendStatus(400);
+        res.status(400).json(err);
       });
   },
 
